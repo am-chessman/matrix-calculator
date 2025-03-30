99547504f139f9deaacba49d0843668318c9790d
chessman <march.chessman@git.com>
2025-03-30 21:16:25 +0200

    Left with organising code into functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,43 @@
 
 using namespace std;
 
+void sumOfMatrix()
+{
+    int mat[2][2];
+    cout << "Sum of the matrices:\n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << mat[i][j] + mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void productOfMatrix()
+{
+    int mat[2][2];
+    cout << "Sum of the matrices:\n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << mat[i][j] * mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void determinantOfMatrix()
+{
+    int mat[2][2];
+    int determinant = 0;
+    cout << "Enter 4 elements for the matrix: \n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> mat[i][j];
+        }
+    }
+    int determinant1 = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
+}
+
 int main() {
     int matrixSize;
 
@@ -42,16 +79,31 @@ int main() {
         cout << endl;
     }
 
-    // Print Matrix 2
-    cout << "Matrix 2:\n";
+    // Calculate and print the sum of the matrices
+    cout << "Sum of the matrices:\n";
     for (int i = 0; i < matrixSize; i++) {
         for (int j = 0; j < matrixSize; j++) {
-            cout << matrix2[i][j] << " ";
+            cout << matrix1[i][j] + matrix2[i][j] << " ";
         }
         cout << endl;
     }
 
-    // Free memory properly
+    // // Calculate and print the product of the matrices
+    cout << "Product of the matrices:\n";
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            cout << matrix1[i][j] * matrix2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    //Determinant
+    int determinant1 = (matrix1[0][0] * matrix1[1][1]) - (matrix1[0][1] * matrix1[1][0]);
+    cout << "Determinant of matrix 1: " << determinant1; 
+
+    int determinant2 = (matrix2[0][0] * matrix2[1][1]) - (matrix2[0][1] * matrix2[1][0]);
+    cout << "Determinant of matrix2: " << determinant2; 
+    // Free memory
     for (int i = 0; i < matrixSize; i++) {
         delete[] matrix1[i];
         delete[] matrix2[i];
